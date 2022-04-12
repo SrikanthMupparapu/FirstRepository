7dc78d0d7fb523b8758a55bb21707f74f8127766
NenavathYugandhar <nenavathvichky27@gmail.com>
2022-04-11 23:33:03 -0700

    demo test1

---
+++ src/demo1/Test.java
@@ -0,0 +1,15 @@
+package demo1;
+
+public class Test 
+{
+   String name = "yugandhar";
+   String name1 = "devi";
+	public static void main(String[] args)
+	{
+		Test d =new Test();
+		System.out.println(d.name);
+		System.out.println(d.name1);
+		System.out.println(d.name+" "+d.name1);
+	}	
+
+}
